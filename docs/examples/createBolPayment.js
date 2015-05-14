300004f99e82ad9cac1101d61af3fb5557e0d3c9
Francisco <f.thiene@gmail.com>
2015-05-14 17:39:47 -0300

    Add create bol usage example

---
+++ docs/examples/createBolPayment.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Gerencianet = require('gn-api-sdk-node');
+var moment = require('moment');
+var credentials = require('./credentials');
+
+var options = {
+  client_id: credentials.client_id,
+  client_secret: credentials.client_secret,
+  sandbox: true
+}
+
+var tomorrow = moment()
+  .add(1, 'days')
+  .format('YYYY-MM-DD 00:00:00');
+
+var paymentInput = {
+  charge_id: 242,
+  payment: {
+    bol: {
+      expire_at: tomorrow
+    }
+  }
+}
+
+var gerencianet = new Gerencianet(options);
+
+gerencianet
+  .createPayment(paymentInput)
+  .then(function (payment) {
+    console.log('Payment:', payment);
+  })
+  .catch(function (err) {
+    console.log('Error:', err);
+  });
