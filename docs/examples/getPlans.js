8989b291d6ed7954fa21935c88599156f8ec971a
Francisco <f.thiene@gmail.com>
2015-07-27 17:04:29 -0300

    Adjust gn-enpoints and add getPlans example

---
+++ docs/examples/getPlans.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Gerencianet = require('gn-api-sdk-node');
+var credentials = require('./credentials');
+
+var options = {
+  client_id: credentials.client_id,
+  client_secret: credentials.client_secret,
+  sandbox: true
+}
+
+var params = {
+  name: 'My Plan',
+  limit: 50,
+  offset: 0
+}
+
+var gerencianet = new Gerencianet(options);
+
+gerencianet
+  .getPlans(params)
+  .then(console.log)
+  .catch(console.log)
+  .done();
