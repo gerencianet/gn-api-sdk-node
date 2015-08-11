f17db26d157b7202f0fed94ae3581350abf0f49f
Cecilia Deveza <ceciliadeveza@gmail.com>
2015-08-11 12:19:26 -0300

    Create the canceling charge example

---
+++ docs/examples/cancelCharge.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Gerencianet = require('gn-api-sdk-node');
+var credentials = require('./credentials');
+
+var options = {
+	client_id: credentials.client_id,
+	client_secret: credentials.client_secret,
+	sandbox: true
+}
+
+var params = {
+	id: 1001
+}
+
+var gerencianet = new Gerencianet(options);
+
+gerencianet
+	.cancelCharge(params)
+	.then(console.log)
+	.catch(console.log)
+	.done();
