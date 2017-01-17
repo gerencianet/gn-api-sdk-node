ae621b8acaff8d1d5f57cf0537b2b478fbd09ab5
Danniel Hugo <dannielhugo@gmail.com>
2017-01-17 14:40:42 -0200

    Remove invalid urls

--- examples/credentials.js
+++ examples/credentials.js
@@ -2,9 +2,5 @@
 
 module.exports = {
 	client_id: 'your_id',
-	client_secret: 'your_secret',
-	urls: {
-		production: 'http://localhost:4400',
-		sandbox: 'http://localhost:4400'
-	}
+	client_secret: 'your_secret'
 }
