e5612dec0ce9e0fd656fba1dec29d2ea8ac0a988
Joao Oliveira <joao.oliveira@gerencianet.com.br>
2021-04-22 18:01:32 -0300

    Melhoria na estrutura do credentials.js

--- examples/credentials.js
+++ examples/credentials.js
@@ -1,11 +1,24 @@
+
+let client_id, client_secret, pathCert; //NÃO PREENCHER AQUI
+
+let sandbox = false; // DEFINA SE ESÁ UTILIZANDO AMBIENTE DE DESENVOLVIMENTO (TRUE) OU PRODUÇÃO (FALSE)
+
+if(sandbox){
+	//  INSIRA ABAIXO SUAS CREDENCIAIS DE DESENVOLVIMENTO E SEU CERTIFICADO
+	client_id = "";
+	client_secret = "";
+	pathCert = "";
+}else{
+	//  INSIRA ABAIXO SUAS CREDENCIAIS DE PRODUÇÃO E SEU CERTIFICADO
+	client_id = "";
+	client_secret = "";
+	pathCert = "";
+}
+
+
 module.exports = {
-	"sandbox": false,
-
-	"client_id_DESENVOLVIMENTO": "",
-	"client_secret_DESENVOLVIMENTO": "",
-	"caminho_certificado_DESENVOLVIMENTO": "",
-
-	"client_id_PRODUCAO": "",
-	"client_secret_PRODUCAO": "",
-	"caminho_certificado_PRODUCAO": ""
+	"sandbox": sandbox,
+	"client_id": client_id,
+	"client_secret": client_secret,
+	"pathCert": pathCert,
 }
