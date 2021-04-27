67f761b6d27d4f06d6753591dd290b97485d444f
Joao Oliveira <joao.oliveira@gerencianet.com.br>
2021-04-27 14:43:19 -0300

    Atualização dos exemplos

--- examples/allInOne.js
+++ examples/allInOne.js
@@ -1,13 +1,8 @@
 'use strict';
 
 var Gerencianet = require('gn-api-sdk-node');
-var credentials = require('./credentials');
+var options = require('./credentials');
 
-var options = {
-  client_id: credentials.client_id,
-  client_secret: credentials.client_secret,
-  sandbox: true
-}
 
 var chargeBody = {
   items: [{
