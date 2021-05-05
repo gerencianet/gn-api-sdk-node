ed0c63b35f3c53d98527ecd07c74f54b28ed3650
Joao Oliveira <joao.oliveira@gerencianet.com.br>
2021-05-05 14:09:03 -0300

    Melhoria do arquivo Credentials

--- examples/credentials.js
+++ examples/credentials.js
@@ -1,24 +1,18 @@
-
-let client_id, client_secret, pathCert; //NÃO PREENCHER AQUI
-
-let sandbox = false; // DEFINA SE ESÁ UTILIZANDO AMBIENTE DE DESENVOLVIMENTO (TRUE) OU PRODUÇÃO (FALSE)
-
-if(sandbox){
-	//  INSIRA ABAIXO SUAS CREDENCIAIS DE DESENVOLVIMENTO E SEU CERTIFICADO
-	client_id = "";
-	client_secret = "";
-	pathCert = "";
-}else{
-	//  INSIRA ABAIXO SUAS CREDENCIAIS DE PRODUÇÃO E SEU CERTIFICADO
-	client_id = "";
-	client_secret = "";
-	pathCert = "";
-}
-
-
 module.exports = {
-	"sandbox": sandbox,
-	"client_id": client_id,
-	"client_secret": client_secret,
-	"pathCert": pathCert,
-}
+	// PRODUÇÃO = false
+	// HOMOLOGAÇÃO = true
+	sandbox: false,
+
+	// CREDENCIAIS DE PRODUÇÃO
+	clientIdProducao: '',
+	clientSecretProducao: '',
+	pathCertProducao: '',
+
+	// CREDENCIAIS DE HOMOLOGAÇÃO
+	clientIdHomologacao: '',
+	clientSecretHomologacao: '',
+	pathCertHomologacao: '',
+
+	// VALIDAR MTLS?
+	validateMtls: false,
+};
